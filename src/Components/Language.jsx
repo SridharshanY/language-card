bbcb9489e19809e3415fef930ae7ac2601cf4d3e
SridharshanY <sridharshany.2001@gmail.com>
2024-08-21 10:56:40 +0530

    finished

--- src/Components/Language.jsx
+++ src/Components/Language.jsx
@@ -1,11 +1,25 @@
-const Language = ({name, img, options}) => {
-    return(
-        <div className="cardWrapper">
-            <img src={img} alt="lang" />
-            <h3>{name}</h3>
-            <p>{options}</p>
-        </div>
-    )
-}
+import { useState } from "react";
 
-export default Language
+const Language = ({ name, img, options }) => {
+  const [show, setShow] = useState(true);
+  console.log(show);
+  return (
+    <div className={`${show ? 'cardWrapper clickBefore':'cardWrapper clickAfter'}`} onClick={() => setShow(!show)}>
+      {show && (
+        <div className="cardBottom">
+          <img src={img} alt="lang" />
+          <h3>{name}</h3>
+        </div>
+      )}
+      {!show && (
+        <ul>
+          {options.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Language;
